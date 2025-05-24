d9f2ecfbbc158c469174981cf3571b4a9dfedf57
mohammadgithub2326 <132339501+mohammadgithub2326@users.noreply.github.com>
2025-05-24 18:15:43 +0530

    Update cartSlice.js

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -1,7 +1,8 @@
+"use client"
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  cartItems: [],
+    cartItems: []
 };
 
 const cartSlice = createSlice({
@@ -15,25 +16,37 @@ const cartSlice = createSlice({
       } else {
         state.cartItems.push({ ...action.payload, quantity: 1 });
       }
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+
     },
     removeFromCart(state, action) {
       const index = state.cartItems.findIndex(item => item.id === action.payload);
       if (index >= 0) {
         state.cartItems.splice(index, 1);
       }
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+
     },
     incrementQty(state, action) {
       const item = state.cartItems.find(item => item.id === action.payload);
       if (item) item.quantity += 1;
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+
     },
     decrementQty(state, action) {
       const item = state.cartItems.find(item => item.id === action.payload);
       if (item && item.quantity > 1) {
         item.quantity -= 1;
       }
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+
     },
+    setCartItems: (state, action) => {
+  state.cartItems = action.payload;
+}
+
   },
 });
 
-export const { addToCart, removeFromCart, incrementQty, decrementQty } = cartSlice.actions;
+export const { addToCart, removeFromCart, incrementQty, decrementQty,setCartItems, } = cartSlice.actions;
 export default cartSlice.reducer;
