49f010a87d60845d10c713b9bbf71e80048d1602
mohammadgithub2326 <132339501+mohammadgithub2326@users.noreply.github.com>
2025-05-24 18:19:27 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from './providers'
+import {Toaster} from 'react-hot-toast';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers> {children}</Providers>
+        <Providers>
+           <Toaster position="top-right" />
+            {children}</Providers>
        
       </body>
        
