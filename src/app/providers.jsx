1fbf68f871f983a5b92031e4ea185ed58699e279
mohammadgithub2326 <132339501+mohammadgithub2326@users.noreply.github.com>
2025-05-24 18:18:10 +0530

    Update providers.jsx

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -2,7 +2,12 @@
 
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
+import { Toaster } from 'react-hot-toast';
 
 export default function Providers({ children }) {
-  return <Provider store={store}>{children}</Provider>;
+  return <Provider store={store}>
+     <Toaster position="top-right" />
+    {children}
+    
+    </Provider>;
 }
